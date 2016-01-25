78fd89f3faf9127b8a2ac9d2705772dcfeeeb811
Brenton "B-Train" Fletcher <i@bloople.net>
2016-01-25 20:36:51 +1100

    Build folder overcomplicates things; Week starts on Monday

--- generate.rb
+++ generate.rb
@@ -5,4 +5,4 @@ template = File.read('index.haml')
 data = JSON.parse(File.read('data.json'))
 haml_engine = Haml::Engine.new(template, { ugly: true, remove_whitespace: true })
 output = haml_engine.render(Object.new, { data: data })
-File.open('build/index.html', 'w') { |f| f << output }
+File.open('index.html', 'w') { |f| f << output }
