3b32bb3e6bf34b0b68676d9436f8400a9d8f285b
Brenton "B-Train" Fletcher <i@bloople.net>
2016-04-07 21:58:40 +1000

    Use fuzzy comparison for same week match (to account for timezone differences)

--- build/times.js
+++ build/times.js
@@ -1,17 +1,18 @@
-// Returns the ISO week of the date.
-Date.prototype.getWeek = function() {
-  var date = new Date(this.getTime());
-  date.setHours(0, 0, 0, 0);
-  // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  // January 4 is always in week 1.
-  var week1 = new Date(date.getFullYear(), 0, 4);
-  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
-}
-
 Date.prototype.isSameDate = function(b) {
   return this.getDate() == b.getDate() && this.getMonth() == b.getMonth() && this.getFullYear() == b.getFullYear();
+}
+
+Date.prototype.isSameWeekFuzzy = function(b) {
+  var startOfWeek = new Date(b.getTime());
+  startOfWeek.setHours(startOfWeek.getHours(), 0, 0, 0);
+  var day = startOfWeek.getDay() || 7;
+  if(day !== 1) startOfWeek.setHours(-24 * (day - 1));
+  startOfWeek.setHours(-24);
+
+  var endOfWeek = new Date(startOfWeek.getTime());
+  endOfWeek.setHours(24 * 9);
+
+  return this.getTime() >= startOfWeek.getTime() && this.getTime() < endOfWeek.getTime();
 }
 
 Date.prototype.formatDate = function(today) {
@@ -52,7 +53,7 @@ function parseTimes() {
     if(scheduledEnd.getTime() < now.getTime()) matchElement.classList.add("past");
     if((now.getTime() >= earlyStart) && (now.getTime() <= scheduledEnd)) matchElement.classList.add("current");
     if(time.isSameDate(now)) matchElement.classList.add("today");
-    if(time.getWeek() == now.getWeek()) matchElement.classList.add("current-week");
+    if(time.isSameWeekFuzzy(now)) matchElement.classList.add("current-week");
     if(earlyStart.getTime() >= now.getTime()) matchElement.classList.add("future");
   }
 }
