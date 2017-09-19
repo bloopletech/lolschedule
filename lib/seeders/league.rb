9d3c9a3deb40671dd18a673d1764b64d972f3d95
Brenton "B-Train" Fletcher <i@bloople.net>
2017-09-19 11:06:02 +1000

    Re-order leagues so the list in the settings modal is ordered by most relevant to least relevant

--- lib/seeders/league.rb
+++ lib/seeders/league.rb
@@ -1,16 +1,16 @@
 class Seeders::League
   LEAGUES = {
-    '10' => 'MSI',
+    '1' => 'All-Star',
+    '9' => 'Worlds',
     '43' => 'Rivals',
+    '10' => 'MSI',
     '2' => 'NA LCS',
     '3' => 'EU LCS',
     '6' => 'LCK',
     '7' => 'LPL',
     '8' => 'LMS',
     '4' => 'NA CS',
-    '5' => 'EU CS',
-    '9' => 'Worlds',
-    '1' => 'All-Star'
+    '5' => 'EU CS'
   }
 
   def initialize(source)
